3a5fd2b4dbaa38668a736c3023bed4ab6bcf9b1e
wilson nie <wilson.nie13@gmail.com>
2021-12-26 16:04:03 -0800

    using hash lib to give blocks signature

--- src/Block/block.py
+++ src/Block/block.py
@@ -1,5 +1,6 @@
 from typing import List 
 import hashlib
+import pickle
 
 class block:
   __perviouse_hash: str
@@ -9,7 +10,8 @@ class block:
   def __init__(self, previous_hash: str, transactions: List[str]):
     self.__perviouse_hash = previous_hash
     self.__transactions = transactions
-    self.__block_hash = hashlib.sha256()
+    self.__block_hash = hashlib.sha256(pickle.dumps(previous_hash) + pickle.dumps(transactions))
   
   def get_block_hash (self) -> str:
     return  self.__block_hash
+  
--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from Block.block import block
 
+myblock: block = block('abc', ['abd', 'dcda'])
 
-myblock: block = block('abc', ['abc', 'dcda'])
-
-
-print(dir(myblock.get_block_hash()))
+print(myblock.get_block_hash().hexdigest())
